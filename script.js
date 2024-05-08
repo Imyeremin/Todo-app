324d3835f565fbf18b1048e760e68e2505581f0c
imyeremin <eremipsch@mail.ru>
2024-05-08 19:57:01 +0300

    Подключение localStorage для хранения tasks

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ const addTaskBtn = document.getElementById("add-task-btn");
 const deskTaskInput = document.getElementById("description-task");
 const todosWrapper = document.querySelector(".todos-wtapper");
 
-// const task = {
-//     description: 'сходить погулять',
-//     complited: false
-// }
+var tasks;
 
-var tasks = [];
+!localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks')); 
 
 function Task(description){
     this.description = description;
     this.complited = false;
 }
 
+const updateLocal = () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks) )
+}
+
 addTaskBtn.addEventListener('click',() => {
    tasks.push(new Task(deskTaskInput.value));
-   console.log(tasks)
+   updateLocal();
 } )
+
