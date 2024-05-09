812ee28eed4faa042fb21a9577f2b13167cd022e
imyeremin <eremipsch@mail.ru>
2024-05-09 14:12:43 +0300

    добавили функционал completed и стилизацию действий

--- script.js
+++ script.js
@@ -3,43 +3,59 @@ const deskTaskInput = document.getElementById("description-task");
 const todosWrapper = document.querySelector(".todos-wtapper");
 
 var tasks;
+
 !localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
 
-function Task(description){
-     this.description = description;
+var todoItemElems = [];
+
+function Task(description){ 
+   this.description = description;
     this.complited = false;
 }
 const createTemplate = (task, index) => {
-    return `
+      return `
     <div class="todo-item ${task.complited ? 'checked' : ''}">
     <div class="description">${task.description} </div>
     <div class="buttons">
-      <input type="checkbox" class="btn-complete" ${task.complited ? 'checked' : ''}> 
+      <input onclick="completeTask(${index});" type="checkbox" class="btn-complete" ${task.complited ? 'checked' : ''}> 
       <button class="btn-delete">Delete</button>
     </div>
   </div>
     `
-}
+   }
 
 const fillHtmlList = () => { 
     todosWrapper.innerHTML = '';
-    if(tasks.length > 0){ 
-     tasks.forEach((item, index) => {
-         todosWrapper.innerHTML += createTemplate(item, index); 
-         });
+    if(tasks.length > 0){  tasks.forEach((item, index) => {
+            todosWrapper.innerHTML += createTemplate(item, index); 
+               });
+      todoItemElems = document.querySelectorAll(".todo-item")
     }
 }
-fillHtmlList(); 
+fillHtmlList();  
+
 const updateLocal = () => {
     localStorage.setItem('tasks', JSON.stringify(tasks) )
-    }
+   }
+
+const completeTask = (index) => {
+    console.log(index)
+   tasks[index].complited = !tasks[index].complited
+    if(tasks[index].complited){
+    todoItemElems[index].classList.add('checked')
+   }else{
+    todoItemElems[index].classList.remove('checked')
+   }
+   updateLocal();
+   fillHtmlList();
+}
 
 addTaskBtn.addEventListener('click',() => {
-     tasks.push(new Task(deskTaskInput.value));
-      updateLocal();
+      tasks.push(new Task(deskTaskInput.value));
+     updateLocal();
    fillHtmlList();
-     deskTaskInput.value = '';
-   } )
+      deskTaskInput.value = '';
+   })
 
 
 
