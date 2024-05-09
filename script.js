8953ca284d6d48ed88057a44ca38e665518cd9f6
imyeremin <eremipsch@mail.ru>
2024-05-09 13:23:31 +0300

    Сделаны функции добовления задачи на страницу из localstorage и новых задач

--- script.js
+++ script.js
@@ -3,20 +3,43 @@ const deskTaskInput = document.getElementById("description-task");
 const todosWrapper = document.querySelector(".todos-wtapper");
 
 var tasks;
-
-!localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks')); 
+!localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
 
 function Task(description){
-    this.description = description;
+     this.description = description;
     this.complited = false;
 }
-
-const updateLocal = () => {
-    localStorage.setItem('tasks', JSON.stringify(tasks) )
+const createTemplate = (task, index) => {
+    return `
+    <div class="todo-item ${task.complited ? 'checked' : ''}">
+    <div class="description">${task.description} </div>
+    <div class="buttons">
+      <input type="checkbox" class="btn-complete" ${task.complited ? 'checked' : ''}> 
+      <button class="btn-delete">Delete</button>
+    </div>
+  </div>
+    `
 }
 
+const fillHtmlList = () => { 
+    todosWrapper.innerHTML = '';
+    if(tasks.length > 0){ 
+     tasks.forEach((item, index) => {
+         todosWrapper.innerHTML += createTemplate(item, index); 
+         });
+    }
+}
+fillHtmlList(); 
+const updateLocal = () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks) )
+    }
+
 addTaskBtn.addEventListener('click',() => {
-   tasks.push(new Task(deskTaskInput.value));
-   updateLocal();
-} )
+     tasks.push(new Task(deskTaskInput.value));
+      updateLocal();
+   fillHtmlList();
+     deskTaskInput.value = '';
+   } )
+
+
 
